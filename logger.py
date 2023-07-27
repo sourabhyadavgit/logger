75522401bc6263be7b5c8d28090daba41790e10a
Sourabh Yadav <saurav_sori@yahoo.com>
2023-07-27 15:24:13 +0100

    added logger

---
+++ logger.py
@@ -0,0 +1,22 @@
+import logging
+
+def setup_logger(name, log_file, level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+# Usage example:
+logger = setup_logger('my_api', 'hostapi.log')
+
+# Now you can use the logger to log messages at different levels
+logger.debug('This is a debug message')
+logger.info('This is an info message')
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+logger.critical('This is a critical message')
